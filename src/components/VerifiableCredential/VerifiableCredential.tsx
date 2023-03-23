87c351f8a60ac8be13cf19447069fb8cb43664ca
Simonas Karuzas <simonas@not.cat>
2023-03-23 15:29:16 +0200

    feat: DiscordKudos template

--- src/components/VerifiableCredential/VerifiableCredential.tsx
+++ src/components/VerifiableCredential/VerifiableCredential.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import { VerifiableCredential as VCred} from "@veramo/core";
 import { Kudos } from './Templates/Kudos';
+import { DiscordKudos } from './Templates/DiscordKudos';
 import { Generic } from './Templates/Generic';
 import "./VerifiableCredential.css"
 
 const VerifiableCredential = ({ credential } : { credential: VCred }) => {
   if (credential.type?.includes('Kudos')) {
     return <Kudos credential={credential} />;
+  }
+
+  if (credential.type?.includes('DiscordKudos')) {
+    return <DiscordKudos credential={credential} />;
   }
 
   return <Generic credential={credential} />;
